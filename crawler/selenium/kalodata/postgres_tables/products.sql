751ef659680f0ed0f188dfd0ca1e0159d1d74fa3
Mauricio <mauricio.o.mesquita@gmail.com>
2026-01-13 20:14:34 -0500

    Update the other sql files

--- crawler/selenium/kalodata/postgres_tables/products.sql
+++ crawler/selenium/kalodata/postgres_tables/products.sql
@@ -1,22 +1,22 @@
 CREATE TABLE products (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  country TEXT,
-  k_id TEXT NOT NULL,
-  k_position INTEGER,
   name TEXT NOT NULL,
+  country TEXT,
   launch_date TEXT,
   product_rating NUMERIC(5, 2),
   main_category INTEGER,
   second_category INTEGER,
   third_category INTEGER,
-  creator_conversion_ratio NUMERIC(5, 2),
-  revenue NUMERIC(15, 2),
-  revenue_history NUMERIC[],
-  revenue_growth_rate NUMERIC(5, 2),
-  sales NUMERIC(15, 2),
   unit_price NUMERIC(10, 2),
-  top_creators NUMERIC[], -- 
-  top_videos NUMERIC[], -- 
+  k_id TEXT NOT NULL,
+  k_position INTEGER,
+  k_creator_conversion_ratio NUMERIC(5, 2),
+  k_revenue NUMERIC(15, 2),
+  k_revenue_history NUMERIC[],
+  k_revenue_growth_rate NUMERIC(5, 2),
+  k_sales NUMERIC(15, 2),
+  k_top_creators NUMERIC[], -- 
+  k_top_videos NUMERIC[], -- 
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE
 );
--- crawler/selenium/kalodata/postgres_tables/videos.sql
+++ crawler/selenium/kalodata/postgres_tables/videos.sql
@@ -1,12 +1,12 @@
 CREATE TABLE videos (
   id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-  k_id TEXT NOT NULL, -- id from shop-detail
   tt_account TEXT, -- account from shop-detail
   description TEXT, -- description from shop-detail
   views NUMERIC(15, 2), -- views from shop-detail
   duration TEXT, -- duration from shop-detail
-  revenue NUMERIC(15, 2), -- revenue from shop-detail-total
-  sales NUMERIC(15, 2),
+  k_id TEXT NOT NULL, -- id from shop-detail
+  k_revenue NUMERIC(15, 2), -- revenue from shop-detail-total
+  k_sales NUMERIC(15, 2),
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE
 );
