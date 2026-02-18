c72a61db8fe926f66c082fa1b4a4e89ab0bf786a
Mauricio <mauricio.o.mesquita@gmail.com>
2026-02-17 20:53:41 -0500

    Updating the next.config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_CONVEX_URL!}/**`)],
+    remotePatterns: process.env.NEXT_PUBLIC_CONVEX_URL
+      ? [
+          {
+            protocol: 'https',
+            hostname: new URL(process.env.NEXT_PUBLIC_CONVEX_URL).hostname,
+          },
+        ]
+      : [],
   },
 }
 
