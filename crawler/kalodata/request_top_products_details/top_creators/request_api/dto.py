40def2cce5211e3dfc20e9e2e44ee002aec68f6a
Mauricio <mauricio.o.mesquita@gmail.com>
2026-02-26 21:41:14 -0500

    Sync next-env.d.ts

--- crawler/kalodata/request_top_products_details/top_creators/request_api/dto.py
+++ crawler/kalodata/request_top_products_details/top_creators/request_api/dto.py
@@ -9,6 +9,8 @@ def dto(data):
             'tt_nickname': data['nickname'],
             'tt_followers': int(data['followers']),
             'k_revenue': parse_value(data['revenue']),
+            'k_video_revenue': parse_value(data['video_revenue']),
+            'k_live_revenue': parse_value(data['live_revenue']),
             'k_sales': int(parse_value(data['sale'])),
         })
     return formated_data
